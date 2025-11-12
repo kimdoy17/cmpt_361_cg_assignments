4ce70c019246544cefb1a08b0149cee189daad12
Doyoup (Paul) Kim <kimdoy17@gmail.com>
2025-11-12 11:34:32 -0800

    Update a3.js

--- cmpt361_a3/a3.js
+++ cmpt361_a3/a3.js
@@ -11,29 +11,29 @@ Rasterizer.prototype.drawLine = function(v1, v2) {
   const [x1, y1, [r1, g1, b1]] = v1;
   const [x2, y2, [r2, g2, b2]] = v2;
   // TODO/HINT: use this.setPixel(x, y, color) in this function to draw line
-  let dx = x2 - x1;
-  let dy = y2 - y1;
+  let deltaX = x2 - x1;
+  let deltaY = y2 - y1;
 
   let color1 = [r1, g1, b1];
   let color2 = [r2, g2, b2];
-  let step;
-  if (Math.abs(dx) >= Math.abs(dy)) {
-    step = Math.abs(dx);
+  let numSteps;
+  if (Math.abs(deltaX) >= Math.abs(deltaY)) {
+    numSteps = Math.abs(deltaX);
   }
   else {
-    step = Math.abs(dy);
+    numSteps = Math.abs(deltaY);
   }
-  dx /= step;
-  dy /= step;
+  deltaX /= numSteps;
+  deltaY /= numSteps;
 
   let x = x1
   let y = y1
-  for (let i = 0; i <= step; ++i) {
-    let frac = i / step;
-    let colormix = color_interp(color1, color2, frac)
-    this.setPixel(Math.floor(x), Math.floor(y), colormix);
-    x += dx;
-    y += dy;
+  for (let i = 0; i <= numSteps; ++i) {
+    let interpolationFactor = i / numSteps;
+    let interpolationColor = color_interp(color1, color2, interpolationFactor)
+    this.setPixel(Math.floor(x), Math.floor(y), interpolationColor);
+    x += deltaX;
+    y += deltaY;
   }
 }
 
@@ -80,73 +80,79 @@ const DEF_INPUT = [
   "l,7,4;"
 ].join("\n");
 
-function color_interp(color1,color2,frac) {
-  const [r1,g1,b1] = color1;
-  const [r2,g2,b2] = color2;
-  const r = (r2 - r1) * frac + r1;
-  const g = (g2 - g1) * frac + g1;
-  const b = (b2 - b1) * frac + b1;
-  return [r,g,b]
-}
+const tripleProduct = (x0, y0, x1, y1, px, py) => px*(y0-y1) + py*(x1-x0) + x0*y1-y0*x1;
 
-function barycentricCoordinates(v1, v2, v3, p) {
-  const [x1, y1, [r1,g1,b1]] = v1;
-  const [x2, y2, [r2,g2,b2]] = v2;
-  const [x3, y3, [r3,g3,b3]] = v3;
-  const [px, py] = p;
-  const a1 = tripleProduct(x2,y2,x3,y3,px,py);
-  const a2 = tripleProduct(x3,y3,x1,y1,px,py);
-  const A = tripleProduct(x1, y1, x2, y2, x3, y3);
-  const u = a1 / A; 
-  const v = a2 / A; 
-  const w = 1.0 - u - v;   
-  const r = u * r1 + v * r2 + w * r3;
-  const g = u * g1 + v * g2 + w * g3;
-  const b = u * b1 + v * b2 + w * b3;
-  return [r, g, b]
-}
-
-
-function pointIsInsideTriangle(v1,v2,v3,p){
-  const [x1,y1] = v1;
-  const [x2,y2] = v2;
-  const [x3,y3] = v3;
-  const [px,py] = p;
+function color_interp(c1, c2, t) {
+  const [red1, green1, blue1] = c1;
+  const [red2, green2, blue2] = c2;
   
-  const w1 = tripleProduct(x1, y1, x2, y2, px, py);
-  const w2 = tripleProduct(x2, y2, x3, y3, px, py);
-  const w3 = tripleProduct(x3, y3, x1, y1, px, py);
+  const interpolated_red = red1 + (red2 - red1) * t;
+  const interpolated_green = green1 + (green2 - green1) * t;
+  const interpolated_blue = blue1 + (blue2 - blue1) * t;
+  
+  return [interpolated_red, interpolated_green, interpolated_blue];
+}
 
-  const area = tripleProduct(x1, y1, x2, y2, x3, y3);
+function barycentricCoordinates(vertex1, vertex2, vertex3, point) {
+  const [vx1, vy1, [vr1, vg1, vb1]] = vertex1;
+  const [vx2, vy2, [vr2, vg2, vb2]] = vertex2;
+  const [vx3, vy3, [vr3, vg3, vb3]] = vertex3;
+  const [pointX, pointY] = point;
+  
+  const area1 = tripleProduct(vx2, vy2, vx3, vy3, pointX, pointY);
+  const area2 = tripleProduct(vx3, vy3, vx1, vy1, pointX, pointY);
+  const totalArea = tripleProduct(vx1, vy1, vx2, vy2, vx3, vy3);
+  
+  const lambda1 = area1 / totalArea;
+  const lambda2 = area2 / totalArea;
+  const lambda3 = 1.0 - lambda1 - lambda2;
+  
+  const red = lambda1 * vr1 + lambda2 * vr2 + lambda3 * vr3;
+  const green = lambda1 * vg1 + lambda2 * vg2 + lambda3 * vg3;
+  const blue = lambda1 * vb1 + lambda2 * vb2 + lambda3 * vb3;
+  
+  return [red, green, blue];
+}
 
-  const topLeft1 = isTopLeft(v1, v2);
-  const topLeft2 = isTopLeft(v2, v3);
-  const topLeft3 = isTopLeft(v3, v1);
-
-  if (area > 0) {
-    return (w1 > 0 || (w1 === 0 && topLeft1)) &&
-           (w2 > 0 || (w2 === 0 && topLeft2)) &&
-           (w3 > 0 || (w3 === 0 && topLeft3));
-  } else {        // CW -> signs flip
-    return (w1 < 0 || (w1 === 0 && topLeft1)) &&
-           (w2 < 0 || (w2 === 0 && topLeft2)) &&
-           (w3 < 0 || (w3 === 0 && topLeft3));
+function pointIsInsideTriangle(vert1, vert2, vert3, pt) {
+  const [vx1, vy1] = vert1;
+  const [vx2, vy2] = vert2;
+  const [vx3, vy3] = vert3;
+  const [ptx, pty] = pt;
+  
+  const weight1 = tripleProduct(vx1, vy1, vx2, vy2, ptx, pty);
+  const weight2 = tripleProduct(vx2, vy2, vx3, vy3, ptx, pty);
+  const weight3 = tripleProduct(vx3, vy3, vx1, vy1, ptx, pty);
+  const triangleArea = tripleProduct(vx1, vy1, vx2, vy2, vx3, vy3);
+  
+  const edgeTopLeft1 = isTopLeft(vert1, vert2);
+  const edgeTopLeft2 = isTopLeft(vert2, vert3);
+  const edgeTopLeft3 = isTopLeft(vert3, vert1);
+  
+  if (triangleArea > 0) {
+    return (weight1 > 0 || (weight1 === 0 && edgeTopLeft1)) &&
+           (weight2 > 0 || (weight2 === 0 && edgeTopLeft2)) &&
+           (weight3 > 0 || (weight3 === 0 && edgeTopLeft3));
+  } else {
+    return (weight1 < 0 || (weight1 === 0 && edgeTopLeft1)) &&
+           (weight2 < 0 || (weight2 === 0 && edgeTopLeft2)) &&
+           (weight3 < 0 || (weight3 === 0 && edgeTopLeft3));
   }
 }
 
-
-function isTopLeft (v0,v1) {
-  const [x1, y1] = v0;
-  const [x2, y2] = v1;
-  let x = 0;
-  let y = 1;
-  const edge = [x2 - x1, y2 - y1];
-
-  const is_top_edge = edge[y] === 0 && edge[x] > 0; 
-  const is_left_edge = edge[y] > 0;
-
-  return is_left_edge || is_top_edge;
+function isTopLeft(v_start, v_end) {
+  const [start_x, start_y] = v_start;
+  const [end_x, end_y] = v_end;
+  
+  const X_INDEX = 0;
+  const Y_INDEX = 1;
+  
+  const edgeVector = [end_x - start_x, end_y - start_y];
+  
+  const isTopEdge = edgeVector[Y_INDEX] === 0 && edgeVector[X_INDEX] > 0;
+  const isLeftEdge = edgeVector[Y_INDEX] > 0;
+  
+  return isLeftEdge || isTopEdge;
 }
 
-const tripleProduct = (x0, y0, x1, y1, px, py) => px*(y0-y1) + py*(x1-x0) + x0*y1-y0*x1;
 export { Rasterizer, Framebuffer, DEF_INPUT};
